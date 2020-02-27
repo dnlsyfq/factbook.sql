86a1fa97166df387499f26172da00c8e465ce7f8
Danial Syafiq <dan@Danials-MacBook-Air.local>
2020-02-27 23:37:56 +0800

    updated

--- -- SQLiteQuery.sql
+++ -- SQLiteQuery.sql
@@ -55,11 +55,36 @@
 --     CASE 
 --         WHEN 
 
+-- FOR CAPITAL CITY 
+-- SELECT 
+--     T.country,
+--     T.urban_pop,
+--     T.country_pop,
+--     T.urban_pct
+-- FROM (SELECT 
+--     facts.name country,
+--     facts.population total_pop,
+--     cities.name city ,
+--     cities.population urban_pop,
+--     ROUND(CAST (cities.population AS FLOAT) / CAST( facts.population AS FLOAT),6) AS urban_pct
+-- FROM facts 
+-- INNER JOIN cities ON facts.id = cities.facts_id
+-- WHERE cities.capital = 1) T
+-- WHERE T.urban_pct > 0.5 
+-- ORDER BY 4 ASC;
+
+-- ALL CITY 
 SELECT 
+    T.country,
+    T.urban_pop,
+    T.total_pop,
+    ROUND((CAST(T.urban_pop AS FLOAT) / CAST(T.total_pop AS FLOAT)), 6) urban_pct
+FROM (SELECT 
     facts.name country,
-    facts.population country_pop,
-    cities.name city ,
-    cities.population city_pop
+    facts.population total_pop,
+    SUM(cities.population) urban_pop --ALL CITIES POP SUM 
 FROM facts 
 INNER JOIN cities ON facts.id = cities.facts_id
-WHERE cities.capital = 1
+GROUP BY 1,2) T
+WHERE urban_pct > 0.5
+ORDER BY 4 ASC;
